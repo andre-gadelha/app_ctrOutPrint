32a6f69d8729416f9e09dcccf02f33f00d3dbe0e
andre-gadelha <adrrda@gmail.com>
2025-04-29 16:50:29 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Banner from './componentes/banner/Banner';
+import InputText from './componentes/InputText/Index';
+
+
+
 
 function App() {
   return (
     <div className="App">
+      <Banner />
+      
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <InputText />
       </header>
     </div>
   );
---
+++ src/componentes/InputText/Index.js
@@ -0,0 +1,13 @@
+import './InputText.css'
+
+const InputText = () =>{
+    return(
+        <div className='input-text'>
+            <label>Código</label>
+            <input placeholder='Digite' />
+        </div>       
+    )
+
+}
+
+export default InputText
