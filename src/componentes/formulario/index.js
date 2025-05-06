510dba027677f59732409a8bdf5330dfed1f5cb7
andre-gadelha <adrrda@gmail.com>
2025-05-05 22:11:44 -0300

    Incluindo o componente do botão

--- src/componentes/formulario/index.js
+++ src/componentes/formulario/index.js
@@ -1,4 +1,5 @@
 import InputText from '../InputText/Index'
+import Botao from '../botao/botao'
 import SelectBox from '../selectBox/SelectBox'
 import './Formulario.css'
 
@@ -8,15 +9,21 @@ const Formulario = () => {
     const tipo = ['MONOCROMÁTICA','POLICROMÁTICA']
     const cor = ['CYAN', 'MAGENTA', 'PRETO','YELLO']
 
+    const enviar =(evento) =>{
+        evento.preventDefault()//Não permite recarregar página
+        alert('teste')
+    }
+
     return(
         <section>
-            <form>
-                <InputText label="Código" placeholder="Informe o código" />
+            <form onSubmit={enviar}>
+                <InputText label="Código" placeholder="Informe o código" obrigatorio={true} />
                 <InputText label="Descrição" placeholder="Informe a descrição do Toner" />
                 <InputText label="Nota" placeholder="Informe o código da nota " />
                 <InputText label="Data de Recebimento" placeholder="Informe a data de Recebimento" />
                 <SelectBox label="Tipo" itens={tipo} />
-                <SelectBox label="Cor" itens={cor} />              
+                <SelectBox label="Cor" itens={cor} /> 
+                <Botao>Enviar</Botao>             
             </form>
         </section>
     )
